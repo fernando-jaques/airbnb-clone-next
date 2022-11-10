2de604dba80f94a1266fa160a59f380e733dcdbd
Fernando Jaques <fernando.jaques@outlook.com>
2022-11-10 15:20:06 -0300

    Header

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Clone Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Banner */}
+      <Banner />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
